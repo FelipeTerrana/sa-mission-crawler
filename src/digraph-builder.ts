8490731d783667abbead3d04e84aa5d76d480a52
Felipe Terrana Cazetta <felipetcazetta@gmail.com>
2023-05-07 17:03:45 -0300

    Build graph recursively by traversing asynchronous tree structure

--- src/digraph-builder.ts
+++ src/digraph-builder.ts
@@ -20,7 +20,30 @@ export default class DigraphBuilder {
             throw new Error("Builder must have both digraph and tree set");
         }
 
-        // TODO implement
-        return Promise.resolve(new Digraph());
+        let visited = {};
+        return DigraphBuilder.buildPartial(this.digraph, this.tree, visited);
+    }
+
+
+
+    private static buildPartial(digraph: Digraph, tree: AsyncTree, visited: {[key: string]: boolean}): Promise<Digraph> {
+        if(visited[tree.name]) {
+            return Promise.resolve(digraph);
+        }
+
+        console.log(`Visiting node "${tree.name}"`);
+        visited[tree.name] = true;
+
+        return tree.loadChildren().then(subtrees => {
+            subtrees.forEach(subtree => {
+                digraph.addEdge(tree.name, subtree.name);
+            });
+
+            return subtrees.reduce((promiseAccumulator, currentSubtree) => {
+                return promiseAccumulator.then(digraphAccumulator => 
+                    DigraphBuilder.buildPartial(digraphAccumulator, currentSubtree, visited)
+                );
+            }, Promise.resolve(digraph));
+        });
     }
 }
